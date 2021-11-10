de533398ff294adbf34105836ecd0786fa4f846d
Matheus Guimaraes <matheusguimr@gmail.com>
2021-11-10 15:10:03 -0300

    test: add stock_service unit tests

--- api_service/api/tests.py
+++ api_service/api/tests.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient, APITestCase
 
 from api.models import UserRequestHistory
 from api.serializers import UserRequestHistorySerializer
